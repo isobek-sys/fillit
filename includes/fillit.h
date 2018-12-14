e120412be8f068110fb99558b701216d53943fb3
blukasho <bodik1w@gmail.com>
2018-12-14 13:26:52 +0200

    waiting to valid

--- includes/fillit.h
+++ includes/fillit.h
@@ -6,7 +6,7 @@
 /*   By: blukasho <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/06 14:00:41 by blukasho          #+#    #+#             */
-/*   Updated: 2018/12/13 18:03:32 by blukasho         ###   ########.fr       */
+/*   Updated: 2018/12/13 18:29:29 by blukasho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,15 @@ typedef struct	s_point
 	int	y;
 }				t_p;
 
-int		count_numb_of_tetr(char *maps);
-int		validation(int fd);
-int		check_symbols(char *cont);
-int		len_before_ch(char *str, char ch);
-char	*get_next_tetr(char *tetrs);
-char	**get_arr(int y, int x);
-void	fillit(char *maps);
-void	del_arr(char ***arr);
-void	print_arr(char **arr);
-void	parse_maps(char *maps);
+int				count_numb_of_tetr(char *maps);
+int				validation(int fd);
+int				check_symbols(char *cont);
+int				len_before_ch(char *str, char ch);
+char			*get_next_tetr(char *tetrs);
+char			**get_arr(int y, int x);
+void			fillit(char *maps);
+void			del_arr(char ***arr);
+void			print_arr(char **arr);
+void			parse_maps(char *maps);
 
 #endif
--- srcs/validation.c
+++ srcs/validation.c
@@ -6,7 +6,7 @@
 /*   By: vladuhin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/03 18:53:00 by vladuhin          #+#    #+#             */
-/*   Updated: 2018/12/13 17:59:35 by blukasho         ###   ########.fr       */
+/*   Updated: 2018/12/13 18:27:52 by blukasho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,38 +14,38 @@
 
 static int	check_newline(char *content, int len)
 {
-    int		i;
-    int		j;
+	int		i;
+	int		j;
 
-    i = 4;
-    j = 0;
-    while(i < len)
-    {
-        while (j < 3)
-        {
-            if (content[i] == '\n')
-                i = i + 5;
-            else
-                return(0);
-            j++;
-        }
-        j = 0;
-        if (content[i] == '\n' && content[i + 1] == '\n')
-            i = i + 6;
-        else if (content[i] == '\n' && content[i + 1] == '\0')
-            break ;
-        else
-            return (0);
-    }
-    return (1);
+	i = 4;
+	j = 0;
+	while (i < len)
+	{
+		while (j++ < 3)
+			if (content[i] == '\n')
+				i = i + 5;
+			else
+				return (0);
+		j = 0;
+		if (content[i] == '\n' && content[i + 1] == '\n'
+				&& content[i + 2] == '\0')
+			return (0);
+		if (content[i] == '\n' && content[i + 1] == '\n')
+			i = i + 6;
+		else if (content[i] == '\n' && content[i + 1] == '\0')
+			break ;
+		else
+			return (0);
+	}
+	return (1);
 }
 
 static int	check_other(char *cont)
 {
-	int		sym;
+	int sym;
 
 	if (!*cont)
-		return(0);
+		return (0);
 	sym = 0;
 	while (*cont)
 	{
@@ -60,71 +60,71 @@ static int	check_other(char *cont)
 
 static int	count_hesh(char *content, int len)
 {
-    int		i;
-    int		j;
-    int		count;
+	int i;
+	int	j;
+	int	count;
 
-    i = -1;
-    j = 0;
-    count = 0;
-    while (++i < len)
-    {
-        j++;
-        if (content[i] == '#')
-            count++;
-        if (j == 20)
-        {
-            if (count != 4)
-                return (0);
-            j = 0;
-            count = 0;
-            ++i;
-        }
-    }
-    return (1);
+	i = -1;
+	j = 0;
+	count = 0;
+	while (++i < len)
+	{
+		j++;
+		if (content[i] == '#')
+			count++;
+		if (j == 20)
+		{
+			if (count != 4)
+				return (0);
+			j = 0;
+			count = 0;
+			++i;
+		}
+	}
+	return (1);
 }
 
 static int	count_touching(char *cont, int len)
 {
-    int		i;
-    int		j;
-    int		touch;
+	int	i;
+	int	j;
+	int	touch;
 
-    i = -1;
-    j = 0;
-    touch = 0;
-    while ((++i < len) && (++j <=20))
-    {
-        (cont[i] == '#' && j > 1 && cont[i - 1] == '#') ? touch++ : touch;
+	i = -1;
+	j = 0;
+	touch = 0;
+	while ((++i < len) && (++j <= 20))
+	{
+		(cont[i] == '#' && j > 1 && cont[i - 1] == '#') ? touch++ : touch;
 		(cont[i] == '#' && j < 19 && cont[i + 1] == '#') ? touch++ : touch;
 		(cont[i] == '#' && j > 5 && cont[i - 5] == '#') ? touch++ : touch;
 		(cont[i] == '#' && j < 15 && cont[i + 5] == '#') ? touch++ : touch;
-        if (j == 20)
-        {
-            if (touch != 6 && touch != 8)
-                return (0);
-            j = 0;
-            touch = 0;
-            ++i;
-        }
-    }
-    return (1);
+		if (j == 20)
+		{
+			if (touch != 6 && touch != 8)
+				return (0);
+			j = 0;
+			touch = 0;
+			++i;
+		}
+	}
+	return (1);
 }
 
-int     validation(int fd)
+int			validation(int fd)
 {
-    int     len;
-    char    file_read[MAXFILE];
-    char    *cont;
+	int		len;
+	char	file_read[MAXFILE];
+	char	*cont;
 	char	*tmp;
 
-    len = read(fd, file_read, MAXFILE);
-	file_read[len] = '\0';
-    cont = ft_strnew(len);
+	len = read(fd, file_read, MAXFILE);
+	file_read[len + 1] = '\0';
+	cont = ft_strnew(len);
 	tmp = cont;
-    cont = ft_strsub(file_read, 0, len);
+	cont = ft_strsub(file_read, 0, len);
 	free(tmp);
-    if (!(check_newline(cont, len)) || !(check_other(cont))
+	if (!(check_newline(cont, len)) || !(check_other(cont))
 		|| !(count_hesh(cont, len)) || !(count_touching(cont, len)))
 	{
 		free(cont);
@@ -134,5 +134,5 @@ int     validation(int fd)
 	parse_maps(cont);
 	fillit(cont);
 	free(cont);
-    return (0);
+	return (0);
 }
